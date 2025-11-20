ca33d8fbdac603f7ca64af6cb3a0d93a6e40bdb4
Elizabeth Orozco <elizabeth.orozco.avalos@gmail.com>
2025-11-20 11:59:30 -0600

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,1 +1,23 @@
 console.log('app');
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Dashboard from "./pages/Dashboard";
+import Productos from "./pages/Productos";
+import ProductoForm from "./pages/ProductoForm";
+import Usuarios from "./pages/Usuarios";
+import Reportes from "./pages/Reportes";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/productos" element={<Productos />} />
+        <Route path="/productos/nuevo" element={<ProductoForm />} />
+        <Route path="/usuarios" element={<Usuarios />} />
+        <Route path="/reportes" element={<Reportes />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
